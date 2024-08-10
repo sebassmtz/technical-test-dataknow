cc6414dbaa6178192da1c2ba9fabd88acb1cd247
sebassmtz <sebastian0197333@gmail.com>
2024-08-09 20:33:14 -0500

    Add: Models of Client and Invoice
    Se crearon los modelos de Cliente y Facturas con el ORM de sequailize

---
+++ backend-node/src/models/client.module.ts
@@ -0,0 +1,59 @@
+import {
+  Model,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from "sequelize"
+import { sequelize } from "../helpers/Database"
+
+import { Invoice } from "./invoice.module"
+
+export class Client extends Model<
+  InferAttributes<Client>,
+  InferCreationAttributes<Client>
+> {
+  declare id: CreationOptional<number>
+  declare name: string
+  declare type_identification: string
+  declare number_identification: string
+  declare observations: string
+  declare createdAt: Date
+  declare Invoices?: Invoice[]
+}
+
+Client.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type_identification: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    number_identification: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    observations: {
+      type: DataTypes.STRING(500),
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    tableName: "clients",
+  }
+)
+
+Client.hasMany(Invoice, { foreignKey: 'clientId' })
---
+++ backend-node/src/models/invoice.module.ts
@@ -0,0 +1,79 @@
+import {
+  Model,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from "sequelize"
+import { sequelize } from "../helpers/Database"
+
+import { Client } from "./client.module"
+
+export class Invoice extends Model<
+  InferAttributes<Invoice>,
+  InferCreationAttributes<Invoice>
+> {
+  declare id: CreationOptional<number>
+  declare date: Date
+  declare name_product: string
+  declare price: number
+  declare discount_vvalue: number
+  declare vat_value: number
+  declare total_value: number
+  declare createdAt: Date
+
+  declare clientId: number
+}
+
+Invoice.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    name_product: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    discount_vvalue: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    vat_value: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    total_value: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    // Clave foránea
+    clientId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Client,
+        key: "id",
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    tableName: "products",
+  }
+)
+
+Invoice.belongsTo(Client, { foreignKey: "clientId" })
